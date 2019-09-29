821e38ad9296e18711d80b170cc344ebc3088445
bomul0327 <bomul0327@naver.com>
2019-09-30 00:42:23 +0900

    Command, Event 관련 인터페이스들 추가

---
+++ Assets/Scripts/Interfaces/ICommand.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public interface ICommand
+{
+    /// <summary>
+    /// 커맨드 실행
+    /// </summary>
+    void Execute ();
+}
---
+++ Assets/Scripts/Interfaces/IEventListener.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public interface IEventListener
+{
+    /// <summary>
+    /// 이벤트를 받았을 때, 호출
+    /// </summary>
+    void OnEvent (IEvent e);
+}
