8c533f4a630cc3ef914749e6f6422d78e1879e42
Shin SeockJu <margron@naver.com>
2019-10-03 00:54:27 +0900

    error fix

--- Assets/Scripts/JsonManager.cs
+++ Assets/Scripts/JsonManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -8,65 +9,82 @@ using Newtonsoft.Json.Linq;
 public class JsonManager 
 {
 
-    private List<Dictionary<int, Object>> Cache;
+    private static List<Dictionary<ObjectCode, Object>> Cache;
 
     public void JasonManager()
     {
-        Cache = new List<Dictionary<int, Object>>();
+        Cache = new List<Dictionary<ObjectCode, Object>>();
         //adding categories into list
-        Cache.Add(new Dictionary<int, Object>());
-        Cache.Add(new Dictionary<int, Object>());
-        Cache.Add(new Dictionary<int, Object>());
+        Cache.Add(new Dictionary<ObjectCode, Object>());
+        Cache.Add(new Dictionary<ObjectCode, Object>());
+        Cache.Add(new Dictionary<ObjectCode, Object>());
     }
 
     public static Object toObject (string jsonfile, ObjectCategory Category)
     {
-        Object ob = JsonConvert.DeserializeObject(jsonfile);
-        Cache.IndexOf(Category).Add(ob.ObjectCode , ob);
+        Object ob = (Object)JsonConvert.DeserializeObject(jsonfile);
+        // Cache.IndexOf(Category).Add(ob.ObjectCode , ob);
         return ob;
     }
 
     public static void Remove (ObjectCode code)
     {
-        Cache.IndexOf(FindCategory(code)).Remove(code);
+        // Cache.IndexOf((int)FindCategory(code)).Remove(code);
     }
 
     public static Object Find (ObjectCode code)
     {
-        return Cache.IndexOf(FindCategory(code))[code];
+        // return Cache.IndexOf((int)FindCategory(code))[code];
+        return null;
     }
 
-    public static bool Have (ObjectCode code)
+    public static bool Contains (ObjectCode code)
     {
-        return Cache.IndexOf(FindCategory()).ContainsKey(code);
+        // return Cache.IndexOf((int)FindCategory(code)).ContainsKey(code);
+        return true;
     }
 
-    private enum ObjectCategory
+    public enum ObjectCategory
     {
-        Character, Enemy, Item
+        Character, Enemy, Item, Unknown
     };
 
-    private enum ObjectCode
+    public enum ObjectCode
     {
-
+        Firstcharacter =0, Lastcharacter =9, Firstenemy =10, Lastenemy =99, Firstitem =100, Lastitem =999
+        // string path = Application.StartupPath; 
+        // path += "GameObjectsInfos.csv"
+        // //CSV 파일로 ObjectCode들을 받을 수 있도록
+        // if (!File.Exists(path))
+        // {
+        //     using (StreamReader sr = File.OpenText(path))
+        //     {
+        //         string s;
+        //         while ((s = sr.ReadLine()) != null )
+        //         {
+        //             Console.WriteLine(s);
+        //         }
+        //     }
+        // }
     };
 
-    private ObjectCategory FindCategory (ObjectCode code)
+    private static ObjectCategory FindCategory (ObjectCode code)
     {
         // character code is between 0 and 9
-        if ( code >= 0 && code < 10 )
+        if ( code >= ObjectCode.Firstcharacter && code < ObjectCode.Lastcharacter )
         {
             return ObjectCategory.Character;
         }
         // enemy code is between 10 and 99
-        else if ( code >= 10 && code < 100 )
+        else if ( code >= ObjectCode.Firstenemy && code < ObjectCode.Lastenemy )
         {
             return ObjectCategory.Enemy;
         } 
         // Item code is over 100
-        else if ( code >= 100 )
+        else if ( code >= ObjectCode.Firstitem )
         {
             return ObjectCategory.Item;
         }
+        return ObjectCategory.Unknown;
     }
 }
