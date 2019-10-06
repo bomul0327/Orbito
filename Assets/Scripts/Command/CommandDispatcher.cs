3822dc4954540a89092a30034cfe5922ddaad1d6
Teddytori <skagh0919@naver.com>
2019-10-06 21:28:36 +0900

    CommandDispatcher 초안

---
+++ Assets/Scripts/Command/CommandDispatcher.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CommandDispatcher
+{
+    /// <summary>
+    /// 커맨드를 큐에 넣어서 순차적으로 실행
+    /// </summary>
+    private static Queue<ICommand> commandQueue = new Queue<ICommand>();
+
+    public static void PushCommand(ICommand command)
+    {
+        commandQueue.Enqueue(command);
+    }
+
+    public static void PopCommand()
+    {
+        if (commandQueue.Count != 0)
+            commandQueue.Dequeue().Execute();
+    }
+}
