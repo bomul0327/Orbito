3cade6f7de3e68f2af44663207c43d35072d2f3c
bomul0327 <bomul0327@naver.com>
2019-10-06 22:14:51 +0900

    CommandDispatcher 일부 수정.

--- Assets/Scripts/Command/CommandDispatcher.cs
+++ Assets/Scripts/Command/CommandDispatcher.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Command를 관리하고, 실행하는 클래스
+/// 모든 Command들은 여기를 무조건 통하게 해야 한다.
+/// </summary>
 public static class CommandDispatcher
 {
     /// <summary>
@@ -9,14 +13,22 @@ public static class CommandDispatcher
     /// </summary>
     private static Queue<ICommand> commandQueue = new Queue<ICommand>();
 
-    public static void PushCommand(ICommand command)
+    /// <summary>
+    /// 커맨드를 Dispatcher에 올려놓습니다.
+    /// </summary>
+    public static void Publish(ICommand command)
     {
         commandQueue.Enqueue(command);
     }
 
-    public static void PopCommand()
+    /// <summary>
+    /// 올려놓은 커맨드들을 모두 실행
+    /// </summary>
+    public static void Handle()
     {
-        if (commandQueue.Count != 0)
+        for(int i = 0; i < commandQueue.Count; ++i)
+        {
             commandQueue.Dequeue().Execute();
+        }
     }
 }
