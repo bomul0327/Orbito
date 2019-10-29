ff047509a071103f9ec09172ed9f7ca6d5e9c909
bomul0327 <bomul0327@naver.com>
2019-10-30 01:46:05 +0900

    싱글턴 클래스 주석 추가 및 게임의 진행을 맡을 Game Class 파일 제작

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 게임의 전체적인 흐름을 관리하는 게임 매니저.
+/// </summary>
+public class Game : Singleton<Game>
+{
+    
+}
--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -1,6 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// 싱글턴 클래스를 위한 포맷. 새로 싱글턴 만들어야 하는 이유 없으면 이거 가져다가 쓰세요.
+/// </summary>
+/// <example>
+/// public class AAA: Singleton<AAA>
+/// {
+///     ~~~
+/// }
+/// </example>
 public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
     private static T _instance;
