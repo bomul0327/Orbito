95aaba03a987e2ebc2046ce50f8b740e50a643c3
Shin SeockJu <margron@naver.com>
2019-11-19 00:42:30 +0900

    xml 추가

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
+/// <summary>
+/// 전체적인 AudioSource를 관리하기 위한 Class입니다.
+/// </summary>
 public class SoundManager : Singleton<SoundManager>
 {
     private string audioClipFolderPath = "Sounds\\";
@@ -27,19 +30,25 @@ public class SoundManager : Singleton<SoundManager>
 
         speedAudioMixer = Resources.Load<AudioMixerGroup>("AudioClipSpeedMixer");
     }
+
+    /// <summary>
+    /// 대상 AudioSource를 추가합니다.
+    /// </summary>
+    /// <param name="audioSource">대상 AudioClip 이름</param>
     public void Add(string audioSource)
     {
         if (!soundDict.ContainsKey(audioSource))
         {
             soundDict.Add(audioSource, gameObject.AddComponent<AudioSource>() );
         }
-
+        soundDict[audioSource].outputAudioMixerGroup = speedAudioMixer;
         soundDict[audioSource].clip = Resources.Load<AudioClip>(audioClipFolderPath + audioSource);
     }
+
     /// <summary>
     /// AudioSource가 없다면 생성하고 실행시킵니다.
     /// </summary>
-    /// <param name="audioSource">AudioClip 이름</param>
+    /// <param name="audioSource">대상 AudioClip 이름</param>
     /// <param name="playVolume">실행시킬 Volume값(0~1)</param>
     /// <param name="playPitch">실행시킬 Pitch값; Default : 1</param>
     /// <param name="playSpeed">실행시킬 Speed값; Default : 1</param>
@@ -52,7 +61,7 @@ public class SoundManager : Singleton<SoundManager>
 
         SetVolume(audioSource, playVolume);
         SetPitch(audioSource, playPitch);
-        SetSpeed(audioSource, playSpeed);
+        SetSpeed(playSpeed);
 
         if (!soundDict[audioSource].isPlaying)
         {
@@ -64,14 +73,21 @@ public class SoundManager : Singleton<SoundManager>
             Debug.Log("So only changed volume, pitch, speed.");
         }
     }
+
     /// <summary>
     /// Default Volume, Pitch, Speed로 AudioClip을 실행시킵니다.
     /// </summary>
-    /// <param name="audioSource">AudioClip 이름</param>    
+    /// <param name="audioSource">대상 AudioClip 이름</param>    
     public void Play(string audioSource)
     {
         Play(audioSource, 1.0f, 1.0f, 1.0f);
     }
+
+    /// <summary>
+    /// 특정위치에서 소리를 발생시키고 싶을때 사용합니다.
+    /// </summary>
+    /// <param name="audioSource">대상 AudioClip 이름</param>
+    /// <param name="position">소리를 발생시킬 위치</param>
     public void PlayAt(string audioSource, Vector3 position)
     {
         if (!soundDict.ContainsKey(audioSource))
@@ -80,7 +96,11 @@ public class SoundManager : Singleton<SoundManager>
         }
         AudioSource.PlayClipAtPoint(soundDict[audioSource].clip, position);
     }
-    
+
+    /// <summary>
+    /// BGM을 변경합니다. 현재는 fade-out fade-in 기능이 없이 변경됩니다.
+    /// </summary>
+    /// <param name="audioSource">BGM에 쓰일 대상 AudioClip 이름</param>
     public void SetBGM(string audioSource)
     {
         if ( soundDict.ContainsKey("BGM") )
@@ -91,10 +111,15 @@ public class SoundManager : Singleton<SoundManager>
             soundDict.Add("BGM", gameObject.AddComponent<AudioSource>());
             soundDict["BGM"].clip = Resources.Load(audioClipFolderPath + audioSource) as AudioClip; 
             soundDict["BGM"].loop = true;
+            soundDict["BGM"].playOnAwake = true;
         }
     }
 
-    public void Stop(string audioSource)
+    /// <summary>
+    /// 소리를 일시정지 시킵니다.
+    /// </summary>
+    /// <param name="audioSource">대상 AudioClip 이름</param>
+    public void Pause(string audioSource)
     {
         if (soundDict.ContainsKey(audioSource))
         {
@@ -106,7 +131,11 @@ public class SoundManager : Singleton<SoundManager>
         }
     }
 
-    public void Resume(string audioSource)
+    /// <summary>
+    /// 일시정지된 소리를 다시 재생시킵니다.
+    /// </summary>
+    /// <param name="audioSource">대상 AudioClip 이름</param>
+    public void UnPause(string audioSource)
     {
         if (soundDict.ContainsKey(audioSource))
         {
@@ -118,10 +147,15 @@ public class SoundManager : Singleton<SoundManager>
         }
     }
 
+    /// <summary>
+    /// 더이상 필요없는 AudioSource를 제거합니다.
+    /// </summary>
+    /// <param name="audioSource">대상 Audioclip 이름</param>
     public void Remove(string audioSource)
     {
         if (soundDict.ContainsKey(audioSource))
         {
+            Destroy(soundDict[audioSource]);
             soundDict.Remove(audioSource);
         }
         else
@@ -130,6 +164,10 @@ public class SoundManager : Singleton<SoundManager>
         }
     }
 
+    /// <summary>
+    /// 소리를 음소거 시킵니다.(소리는 계속 재생됩니다)
+    /// </summary>
+    /// <param name="audioSource">대상 Audioclip 이름</param>
     public void Mute(string audioSource)
     {
         if (soundDict.ContainsKey(audioSource))
@@ -142,6 +180,10 @@ public class SoundManager : Singleton<SoundManager>
         }
     }
 
+    /// <summary>
+    /// 음소거된 소리의 음소거를 해제합니다.
+    /// </summary>
+    /// <param name="audioSource">대상 Audioclip 이름</param>
     public void UnMute(string audioSource)
     {
         if (soundDict.ContainsKey(audioSource))
@@ -154,6 +196,9 @@ public class SoundManager : Singleton<SoundManager>
         }
     }
 
+    /// <summary>
+    /// 모든 소리를 음소거시킵니다.
+    /// </summary>
     public void MuteAll()
     {
         foreach (var item in soundDict)
@@ -162,6 +207,9 @@ public class SoundManager : Singleton<SoundManager>
         }
     }
 
+    /// <summary>
+    /// 모든 소리를 음소거 해제시킵니다.
+    /// </summary>
     public void UnMuteAll()
     {
         foreach (var item in soundDict)
@@ -170,6 +218,11 @@ public class SoundManager : Singleton<SoundManager>
         }
     }
 
+    /// <summary>
+    /// 해당 소리의 재생여부에 대한 bool값을 return합니다.
+    /// </summary>
+    /// <param name="audioSource">대상 Audioclip 이름</param>
+    /// <returns></returns>
     public bool isPlaying(string audioSource)
     {
         if (soundDict.ContainsKey(audioSource))
@@ -182,6 +235,11 @@ public class SoundManager : Singleton<SoundManager>
         }
     }
 
+    /// <summary>
+    /// 해당 소리의 볼륨을 조절합니다.
+    /// </summary>
+    /// <param name="audioSource">대상 Audioclip 이름</param>
+    /// <param name="playVolume">원하는 Volume배율</param>
     public void SetVolume(string audioSource, float playVolume)
     {
         if (soundDict.ContainsKey(audioSource))
@@ -194,6 +252,11 @@ public class SoundManager : Singleton<SoundManager>
         }
     }
 
+    /// <summary>
+    /// 해당 소리의 높낮이를 조절합니다.
+    /// </summary>
+    /// <param name="audioSource">대상 Audioclip 이름</param>
+    /// <param name="playPitch">원하는 Pitch 배율</param>
     public void SetPitch(string audioSource, float playPitch)
     {
         if (soundDict.ContainsKey(audioSource))
@@ -214,36 +277,26 @@ public class SoundManager : Singleton<SoundManager>
         }
     }
 
-    public void SetSpeed(string audioSource, float playSpeed)
+    /// <summary>
+    /// 전체 소리의 속도 배율을 조절합니다.
+    /// </summary>
+    /// <param name="playSpeed">원하는 속도 배율</param>
+    public void SetSpeed(float playSpeed)
     {
         // 현재는 하나의 AudioMixerGroup을 사용하기 때문에 Speed를 조정중인 모든 Sound의 Speed가 동시에 조절됩니다.
         // 전체 소리의 속도가 같이 조절되야 할지 개별의 Sound의 속도가 개별적으로 조절되야 할지에 따라 달라져야 합니다.
 
-        if (soundDict.ContainsKey(audioSource))
+        foreach (var item in soundDict)
         {
-            if (soundDict[audioSource].GetComponent<AudioMixerGroup>() == null)
-            {
-                soundDict[audioSource].outputAudioMixerGroup = speedAudioMixer;
-            }
-
-            foreach (var item in soundDict)
-            {
-                float prevPitch = item.Value.pitch;
-
-                speedAudioMixer.audioMixer.GetFloat("Pitch", out float prevPitchBend);
-
-                item.Value.pitch = prevPitch * prevPitchBend / playSpeed;
-            }
-
-            speedAudioMixer.audioMixer.SetFloat("Pitch", 1f / playSpeed);
+            float prevPitch = item.Value.pitch;
+            speedAudioMixer.audioMixer.GetFloat("Pitch", out float prevPitchBend);
+            item.Value.pitch = prevPitch * prevPitchBend / playSpeed;
         }
-        else
-        {
-            Debug.Log("There is no " + audioSource);
-        }
+
+        speedAudioMixer.audioMixer.SetFloat("Pitch", 1f / playSpeed);
     }
 
-    bool ContainsAudioSource(string audioSource)
+    public bool Contains(string audioSource)
     {
         return soundDict.ContainsKey(audioSource);
     }
