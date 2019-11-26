270d3d3a3d538c49446e041b0a552dbb967f61e5
Shin SeockJu <margron@naver.com>
2019-11-27 00:47:25 +0900

    FMOD core API를 열심히 공부중입니다. 아직 소리가 나지않습니다.

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -4,21 +4,38 @@ using UnityEngine;
 
 
 /// <summary>
-/// 전체적인 AudioCli들을 관리하기 위한 Singleton Class입니다.
+/// 전체적인 AudioClip들을 관리하기 위한 Singleton Class입니다.
 /// </summary>
 public class SoundManager : Singleton<SoundManager>
 {
     FMOD.RESULT result;
     FMOD.ChannelGroup channelGroup;
-    FMOD.Channel[] channels;
     FMOD.System system;
-    FMOD.Sound sound;
+    Dictionary<string, FMOD.Sound> soundDict;
     void Start()
     {
-        FMOD.Factory.System_Create(out system);
-        // 현재 MaxChannel 개수는 임의로 8로 넣었습니다. 아직 도입 과정이라 channel이 몇개 필요할지 모르겠네요.
+        soundDict = new Dictionary<string, FMOD.Sound>();
+
+        result = FMOD.Factory.System_Create(out system);
+        if (result != FMOD.RESULT.OK)
+        {
+            Debug.LogAssertionFormat(string.Format("FMOD error! {0} {1}", result, FMOD.Error.String(result)));
+        }
+
+        // 현재 MaxChannel 개수는 임의로 넣었습니다. 아직 도입 과정이라 channel이 몇개 필요할지 모르겠네요.
         // 나머지는 기본값입니다.
-        system.init(8, FMOD.INITFLAGS.NORMAL, System.IntPtr.Zero);
+        result = system.init(100, FMOD.INITFLAGS.NORMAL, System.IntPtr.Zero);
+        if (result != FMOD.RESULT.OK)
+        {
+            Debug.LogAssertionFormat(string.Format("FMOD error! {0} : {1}", result, FMOD.Error.String(result)));
+        }
+
+
+        result = LoadOnStart();
+        if (result != FMOD.RESULT.OK)
+        {
+            Debug.LogAssertionFormat(string.Format("FMOD error! {0} : {1}", result, FMOD.Error.String(result)));
+        }
     }
 
     /// <summary>
@@ -29,7 +46,38 @@ public class SoundManager : Singleton<SoundManager>
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            SoundManager.Instance.PlaySFX("LaserSample");
+            PlaySFX("LaserSample1.wav");
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Load("Master");
+        }
+
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+        }
+    }
+
+    void AddSound(string audioClip)
+    {
+        if (!soundDict.ContainsKey(audioClip))
+        {
+            FMOD.Sound sound;
+            result = system.createSound(Application.streamingAssetsPath + audioClip, FMOD.MODE._2D | FMOD.MODE.LOOP_OFF, out sound);
+            if (result != FMOD.RESULT.OK)
+            {
+                Debug.Log("The sound is not in dictionary but failed to createsound.");
+                Debug.LogAssertionFormat(string.Format("FMOD error! {0} : {1}", result, FMOD.Error.String(result)));
+            }
+
+            soundDict.Add(audioClip, sound);
+
+            Debug.Log("The sound is not in dictionary and added successfuly.");
+        }
+        else
+        {
+            Debug.Log("The sound is already in dictionary.");
         }
     }
 
@@ -42,7 +90,14 @@ public class SoundManager : Singleton<SoundManager>
     /// <param name="playSpeed">실행시킬 Speed값; Default : 1</param>
     public void PlaySFX(string audioClip, float playVolume = 1.0f, float playPitch = 1.0f, float playSpeed = 1.0f)
     {
-        FMODUnity.RuntimeManager.PlayOneShot("event:/"+audioClip);
+        AddSound(audioClip);
+        FMOD.Channel channel;
+        system.playSound(soundDict[audioClip], channelGroup, true, out channel);
+        channel.setVolume(playVolume);
+        channel.setPitch(playPitch);
+        channel.setFrequency(playSpeed/playPitch);
+
+        channel.setPaused(false);
     }
 
     /// <summary>
@@ -51,7 +106,7 @@ public class SoundManager : Singleton<SoundManager>
     /// <param name="audioClip">변경될 BGM audioClip 이름</param>
     public void SetBGM(string audioClip)
     {
-
+        
     }
 
     /// <summary>
@@ -163,5 +218,39 @@ public class SoundManager : Singleton<SoundManager>
     public void SetSpeed(string audioClip, float playSpeed)
     {
 
+    }
+
+    /// <summary>
+    /// 게임시작시에 미리 memory에 올려놓을 자주쓰고 크기가 작은 audioclip들을 불러옵니다.
+    /// </summary>
+    /// <returns></returns>
+    private FMOD.RESULT LoadOnStart()
+    {
+        // To do
+
+        return FMOD.RESULT.OK;
+    }
+
+    /// <summary>
+    /// Runtime시에 동적으로 audioClip을 memory에 올리기 위한 함수입니다.
+    /// </summary>
+    /// <param name="audioClip">대상 audioClip 이름</param>
+    /// <returns></returns>
+    public FMOD.RESULT Load(string audioClip)
+    {
+        // To do
+        FMODUnity.RuntimeManager.LoadBank(audioClip);
+        return FMOD.RESULT.OK;
+    }
+
+    /// <summary>
+    /// Runtime시에 동적으로 audioClip을 memory에서 내리기 위한 함수입니다.
+    /// </summary>
+    /// <param name="audioClip">대상 audioClip 이름</param>
+    /// <returns></returns>
+    public FMOD.RESULT UnLoad(string audioClip)
+    {
+        // To do
+        return FMOD.RESULT.OK;
     }
 }
