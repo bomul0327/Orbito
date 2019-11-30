e6399ccdcabd76ac910bebfc75bb4635331fb252
정세미 <aorhqq@gmail.com>
2019-11-30 17:59:49 +0900

    Rotate, Revolve 구현, MoveFront 변경

--- Assets/Scripts/Character/CharacterBehaviour.cs
+++ Assets/Scripts/Character/CharacterBehaviour.cs
@@ -19,15 +19,41 @@ public class CharacterBehaviour
         charTransform = character.transform;
     }
 
-
+    /// <summary>
+    /// character의 MoveSpeed로, character의 y축으로 이동
+    /// </summary>
     public void MoveFront()
     {
-        charTransform.position += character.MoveSpeed * charTransform.right * Time.deltaTime;
+        charTransform.Translate(0, character.MoveSpeed * Time.deltaTime, 0);
     }
 
-    public void Rotate(Vector3 center, float radius)
+    /// <summary>
+    /// 공전을 시작했을 때, 공전하는 방향을 character가 바라보게 한다.
+    /// </summary>
+    /// <param name="planetPos"></param>
+    public void Rotate(Vector3 planetPos)
     {
-        Debug.Log("Rotate");
+        var dir = (planetPos - charTransform.position).normalized;
+        var antiClockwiseDir = Vector2.Perpendicular(charTransform.position - charTransform.up).normalized;
+        if (Vector2.Dot(antiClockwiseDir, charTransform.position) > 0) dir = -dir;
+
+        float angle = Mathf.Acos(dir.x) * Mathf.Rad2Deg;
+        if (dir.y < 0) angle *= -1;
+        charTransform.localRotation = Quaternion.Euler(0, 0, angle);
+    }
+
+    /// <summary>
+    /// planetPos를 중심으로 MoveSpeed를 각속도로 변환한 속도로 공전
+    /// </summary>
+    /// <param name="planetPos"></param> character가 공전하는 행성의 위치
+    public void Revolve(Vector3 planetPos)
+    {
+        float deg = (character.MoveSpeed / Vector3.Distance(planetPos, charTransform.position)) * Mathf.Rad2Deg * Time.deltaTime;
+
+        var antiClockwiseDir = Vector2.Perpendicular(charTransform.position - planetPos).normalized;
+        if (Vector2.Dot(antiClockwiseDir, charTransform.up) > 0) deg = -deg;
+
+        charTransform.RotateAround(planetPos, -Vector3.forward, deg);
     }
 
     public void Attack()
