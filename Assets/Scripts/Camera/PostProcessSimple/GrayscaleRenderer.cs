69de704a925ad91ff68ae7bfd0fb446f4e289b8e
devjhin <jhin0904@gmail.com>
2020-01-06 20:47:54 +0900

    PostProcessSimple 추가
    Grayscale PostProcessEffect를 적용할 수 있는  PostProcessRenderer 추가. 더 많은 효과를 효율적으로 적용할 수 있는 PostProcessStack 시스템 추후 개발 필요.

---
+++ Assets/Scripts/Camera/PostProcessSimple/GrayscaleRenderer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class GrayscaleRenderer : MonoBehaviour
+{
+    [Range(0, 1)] public float amount = 1;
+
+    private Material material;
+    private static readonly string shaderPath = "Hidden/PostProcessing/Grayscale";
+    Material Material
+    {
+        get
+        {
+            if (material)
+                return material;
+
+            var shader = Shader.Find(shaderPath);
+            if (shader == null)
+                throw new System.ArgumentException($"Could not find {shaderPath}");
+
+            material = new Material(shader);
+            material.hideFlags = HideFlags.HideAndDontSave;
+
+            return material;
+        }
+    }
+
+    public void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Material.SetFloat("_Amount", amount);
+        Graphics.Blit(source, destination, material);
+    }
+
+
+}
