4929b653d0562802118b8a0c8fafce04b583b9d2
hunny7434 <55790304+hunny7434@users.noreply.github.com>
2020-03-14 10:59:48 +0900

    Update Planet.cs
     공전가능한 행성인지 구별하는 변수, 소형, 중형, 대형 부유물을 구분, 자원을 드랍하는 기능 추가

--- Assets/Scripts/Planet/Planet.cs
+++ Assets/Scripts/Planet/Planet.cs
@@ -7,17 +7,60 @@ using UnityEngine;
 /// </summary>
 public class Planet : FieldObject
 {
-    public float MoveSpeed = 10;
+    public float Radius;
+
     public PlanetBehaviour Behaviour
     {
         get;
         private set;
     }
 
+    public enum Size
+    {
+        small,
+        medium,
+        big
+    }
+
+    private bool selected;
+
+    public bool Selectable;
+
+    public float MoveSpeed = 10;
+
+    public float GravityScale;
+
+    public class Resources
+    {
+        public float CurrentResorces;
+        public float MaxResources;
+    }
+
+    /// <summary>
+    /// 부유물
+    /// </summary>
+    public struct FloatingMatter
+    {
+        public float InnerRadius;
+
+        public int CurrentHP;
+
+        public int MaxHP;
+
+        public class Resources
+        {
+            public float CurrentResorces;
+            public float MaxResources;
+        }
+
+        public float MoveSpeed;
+    }
+
     private void Awake()
     {
-        // Get Behaviour by something.
+        // Get Controller and Behaviour by something.
         // Json 데이터가 준비되면 Json을 통해서 받아올 것
+        // Controller = ?;
         // Behaviour = ?;
     }
 }
